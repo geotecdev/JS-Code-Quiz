a6a9af01c089038d7de1db8877ba82be9ab9d9ec
Mike Ruane <mikeruanedev@gmail.com>
2021-01-16 01:42:46 -0500

    add coverContainer toggle element over quiz area

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,35 @@
-//
+var coverContainer;
+var startBtn;
+
+// event listener to make sure all elements have loaded
+document.addEventListener("DOMContentLoaded", function(event){
+
+    //set element variable values
+    coverContainer = document.querySelector("#coverContainer");
+    coverContainer.style.display  = "none";
+    startBtn = document.querySelector("#startBtn");
+
+    //event listeners
+    document.querySelector("#startBtn").addEventListener("click", hideContentCover);
+
+
+});
+
+
+
+
+
+
+//functions
+
+function hideContentCover() {
+    coverContainer.style.display = "block";
+};
+
+function showContentCover() {
+    coverContainer.style.display = "none";
+}
+
+
+
+
